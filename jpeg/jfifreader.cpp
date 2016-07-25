07cfb0775f15afd6a0ed17da5f8ef7c523ddcbdf
armando <rsb987@hotmail.com>
2016-07-24 22:16:57 -0500

    reads string in header

--- jpeg/jfifreader.cpp
+++ jpeg/jfifreader.cpp
@@ -2,32 +2,79 @@
 
 #include <iostream>
 #include <fstream>
-
+#include <stdexcept>
 
 
 JfifReader::JfifReader(const std::string &a):
     pathFile(a)
 {
+    thumbnail =  nullptr;
+}
+
+JfifReader::~JfifReader()
+{
+    if(thumbnail){
+        delete []thumbnail;
+    }
+}
+
+void JfifReader::validateFile(istream &stream)
+{
+    uint2 marker;
+    read2bytes(stream, marker);
+
+    if(marker != JpegMarker::SOI)
+        std::logic_error("No jfif file in header");
+
+
+    read2bytes(stream, marker);
+
+    if(marker != JpegMarker::APP0)
+        std::logic_error("No app0 marker in header");
+
+    read2bytes(stream, marker);
+
+    char jfifstring[5];
+
+    stream.read(jfifstring, 5);
+
+    if(stream.gcount() < 5)
+         std::logic_error("No jfif string in header");
+
+    if(jfifstring[0] != 'J' || jfifstring[1] != 'F' ||
+       jfifstring[2] != 'I' ||  jfifstring[3] != 'F' ||  jfifstring[4] != 0)
+         std::logic_error("No jfif string in header");
+
 
 }
 
 void JfifReader::readHeader()
 {
     std::ifstream stream(pathFile.c_str(), std::ios::binary);
+    this->validateFile(stream);
 
     uint2 marker;
-    stream>>marker;
 
-    if(marker == JpegMarker::SOI){
-        std::cout<<" START OF IMAGE "<<'\n';
+    read2bytes(stream, marker);
+
+    if(marker == JpegMarker::APP0){
+        //TODO read extra app0 data
+        std::logic_error("read extra app0 header is missing");
     }
+
+    versionMajorId = (char)((marker & 0xFF00)>>8);
+    versionMinorId = (char)(unsigned char)(marker & 0x00FF);
 
 
 }
 
 void JfifReader::read2bytes(istream &stream, uint2 &outbytes)
 {
-    char bytes[2];
+    unsigned char bytes[2];
+
+    stream.read((char*)bytes, 2);
+
+    outbytes = ((int)(bytes[0]) << 8 ) + bytes[1];
 
 }
 
--- jpeg/jfifreader.h
+++ jpeg/jfifreader.h
@@ -14,8 +14,25 @@ class JfifReader
 {
 private:
     string pathFile;
+
+    char versionMajorId;
+    char versionMinorId;
+    char units;
+
+    uint2 xDensity;
+    uint2 yDensity;
+
+    char xThumbnail;
+    char yThumbnail;
+
+    char *thumbnail;
+
+
+
 public:
     JfifReader(const std::string &a);
+    ~JfifReader();
+    void validateFile(std::istream &);
     void readHeader();
     void read2bytes(std::istream &, uint2 &outbytes);
 };
